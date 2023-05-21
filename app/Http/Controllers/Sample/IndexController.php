29b5d0789b62361ffa32a9d01f36e6248425752b
Apple <apple@appurunoMacBook-Pro.local>
2023-05-21 11:25:39 +0900

    modified Index & add migrate file

--- app/Http/Controllers/Sample/IndexController.php
+++ app/Http/Controllers/Sample/IndexController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\Sample;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Sample;
 
 class IndexController extends Controller
 {
     public function show (){
-        return view('index');
+
+        $sample = Sample::find(1);
+        return view('index',['sample' => $sample]);
    }
 }
